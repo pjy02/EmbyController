80ce4d2f6efa99b7fa3ba79bb3d1efbdd4862021
Randall <84122428+RandallAnjie@users.noreply.github.com>
2025-02-20 15:45:37 +0800

    更新数据库emby_device表，添加deactivate列

---
+++ database/migrations/20250220154336_add_deactivate_column_to_emby_device_table.php
@@ -0,0 +1,13 @@
+<?php
+use think\migration\Migrator;
+use think\migration\db\Column;
+
+class AddDeactivateColumnToEmbyDeviceTable extends Migrator
+{
+    public function change()
+    {
+        $table = $this->table('rc_emby_device');
+        $table->addColumn('deactivate', 'boolean', ['default' => 0, 'comment' => '是否停用'])
+            ->update();
+    }
+}
