ad83f1dc4f59513aff779c4163b1b81af34ab08b
pjy02 <61741153+pjy02@users.noreply.github.com>
2025-08-12 01:39:28 +0800

    feat(事件配置): 在设备状态变更事件中添加会话历史监听器
    添加 SessionHistoryListener 到设备状态变更事件监听器列表，用于记录会话历史信息

--- config/event.php
+++ config/event.php
@@ -7,6 +7,7 @@ return [
         \app\media\event\DeviceStatusChangedEvent::class => [
             \app\media\listener\UpdateDeviceDisplayListener::class,
             \app\media\listener\DeviceHistoryListener::class,
+            \app\media\listener\SessionHistoryListener::class,
         ],
     ],
     
